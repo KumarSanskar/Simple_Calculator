063e5bae0ee64b66ce31bf7c23d5ce67f3a24c14
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-11 01:34:04 +0530

    similarly made for the subtract, multiply and divide buttons

--- app/src/main/java/sanskar/kumar/kumarsanskar/simplecalculator/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/simplecalculator/MainActivity.java
@@ -37,8 +37,45 @@ public class MainActivity extends AppCompatActivity {
                 String resultValue = Float.toString(resultantValue);
                 resultDisplay.setText(resultValue);
                 Toast.makeText(MainActivity.this, "The answer is: " + resultValue, Toast.LENGTH_SHORT).show();
-
-
+            }
+        });
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstInput = firstNumber.getText().toString();
+                String secondInput = secondNumber.getText().toString();
+                float firstValue = Float.parseFloat(firstInput);
+                float secondValue = Float.parseFloat(secondInput);
+                float resultantValue = firstValue - secondValue;
+                String resultValue = Float.toString(resultantValue);
+                resultDisplay.setText(resultValue);
+                Toast.makeText(MainActivity.this, "The answer is: " + resultValue, Toast.LENGTH_SHORT).show();
+            }
+        });
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstInput = firstNumber.getText().toString();
+                String secondInput = secondNumber.getText().toString();
+                float firstValue = Float.parseFloat(firstInput);
+                float secondValue = Float.parseFloat(secondInput);
+                float resultantValue = firstValue * secondValue;
+                String resultValue = Float.toString(resultantValue);
+                resultDisplay.setText(resultValue);
+                Toast.makeText(MainActivity.this, "The answer is: " + resultValue, Toast.LENGTH_SHORT).show();
+            }
+        });
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstInput = firstNumber.getText().toString();
+                String secondInput = secondNumber.getText().toString();
+                float firstValue = Float.parseFloat(firstInput);
+                float secondValue = Float.parseFloat(secondInput);
+                float resultantValue = firstValue / secondValue;
+                String resultValue = Float.toString(resultantValue);
+                resultDisplay.setText(resultValue);
+                Toast.makeText(MainActivity.this, "The answer is: " + resultValue, Toast.LENGTH_SHORT).show();
             }
         });
     }
