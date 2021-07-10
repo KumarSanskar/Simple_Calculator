ae865fb4dfae02ccb3a4e13e7f16aa6b0f10b4ea
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-11 00:41:51 +0530

    add objects for class EditText, Button and TextView

--- app/src/main/java/sanskar/kumar/kumarsanskar/simplecalculator/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/simplecalculator/MainActivity.java
@@ -3,12 +3,19 @@ package sanskar.kumar.kumarsanskar.simplecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText firstNumber, secondNumber;
+    TextView resultDisplay;
+    Button addButton, subtractButton, multiplyButton, divideButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
